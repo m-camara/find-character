574e1e5de027e5bf5adec64994197f24f9e60ad3
m-camara <m.camaradwwm@gmail.com>
2025-08-04 15:40:26 +0200

    ajout des personnage peaufinement du site

--- script.js
+++ script.js
@@ -5,45 +5,31 @@ const personnages = [
 
     {nom : "Isaac Clark", image : "images/isaac.webp", jeux : "Dead Space"},
 
-
-
     {nom: "2b", image : "images/2b.webp",jeux: "Nier Automata"},
-
-
 
     {nom: "Ein", image : "images/ein.webp",jeux: "Dead or Alive 2"},
 
-
-
     {nom: "John Shepard", image : "images/johnshepard.webp",jeux: "Mass Effect"},
-
-
 
     {nom: "Judy Alvarez", image : "images/judyalvarez.webp",jeux: "Cyberpunk"},
 
-
-
     {nom: "Misty", image : "images/Misty.webp",jeux: "Black ops 2"},
-
-
 
     {nom: "Panam Palmer", image : "images/panampalmer.webp",jeux: "Cyberpunk"},
 
-
-
     {nom: "Lance Wilson", image : "images/rayder.webp",jeux: "GTA San Andreas"},
-
-
 
     {nom: "Tank Dempsey", image : "images/Tankdempsey.webp",jeux: "Black ops 2"},
 
-
-
     {nom: "Vito Scaletta", image : "images/vitoscaletta.webp",jeux: "Mafia 2"},
 
-
+    {nom: "Elie", image : "images/Elie.webp",jeux: "The Last Of Us Part II"},
+    
+    {nom: "Jett", image : "images/jett.webp",jeux: "Valorant"},
 
     {nom: "Ayane", image : "images/ayane.webp",jeux: "Dead or Alive"},
+
+    {nom: "Abby", image : "images/abby.webp",jeux: "The Last Of Us Part II"},
 
 
 ]
@@ -55,6 +41,7 @@ const input = document.getElementById('championInput');
 const result = document.getElementById('resultat');
 const attemptList = document.getElementById('attemptsList');
 const imageperso = document.querySelector('.imageperso');
+const titreJeu = document.getElementById("titrejeu");
 
 image.src = personnagesRandom.image;
 
@@ -69,7 +56,8 @@ function trouvePersonnages(){
     if(guess.toLowerCase() === personnagesRandom.nom.toLowerCase()){
         result.textContent = `Bravo vous avez trouvé ${personnagesRandom.nom} `;
         result.style.color = "green";
-
+        titreJeu.textContent = `Jeu : ${personnagesRandom.jeux}`;
+        titreJeu.color = "white";
         imageperso.classList.add("revealed");
         input.disabled = true;
     }else{
@@ -90,6 +78,7 @@ function recommencerJeu(){
     image.src = personnagesRandom.image;
     result.textContent = ""
     attemptList.innerHTML = ""
+    titreJeu.textContent = ""
 
     input.disabled = false;
     input.value = "";
